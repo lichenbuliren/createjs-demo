3ec4053db4fc086256945f331ee536d2d63661fb
lichenbuliren <617106268@qq.com>
2015-10-23 18:47:15 +0800

    upate

--- js/Button.js
+++ js/Button.js
@@ -1,13 +1,46 @@
 (function() {
-    function Button(label) {
+    function Button(label, color) {
         this.Container_constructor();
+
         this.label = label;
+        this.color = color;
+
+        this.setup();
     }
 
     var p = createjs.extend(Button, createjs.Container);
 
-    p.draw = function() {
-        this.Container_draw();
+    p.setup = function() {
+        var text = new createjs.Text(this.label, '20px Arial', '#000');
+        text.Baseline = 'top';
+        text.textAlign = 'center';
+
+        var width = text.getMeasuredWidth() + 30;
+        var height = text.getMeasuredHeight() + 20;
+
+        text.x = width / 2;
+        text.y = 10;
+
+        var background = new createjs.Shape();
+        background.graphics.beginFill(this.color).drawRoundRect(0, 0, width, height, 10);
+
+        this.addChild(background, text);
+        this.on('click', this.handleClick);
+        this.on('rollover', this.handleRollOver);
+        this.on('rollout', this.handleRollOver);
+        this.cursor = 'pointer';
+
+        this.mouseChildren = false;
+        this.offset = Math.random() * 10;
+        this.count = 0;
+    }
+
+    p.handleClick = function(event) {
+        alert('你点击了一个按钮：' + this.label);
+    }
+
+    p.handleRollOver = function(event) {
+        this.alpha = event.type == 'rollover' ? 0.4 : 1;
     }
 
     window.Button = createjs.promote(Button, 'Container');
